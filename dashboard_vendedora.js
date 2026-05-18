1e1349b12d3b56b87d43913be9918f5ecf6ff7b9
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-05-18 14:58:35 -0500

    Switch from localStorage to sessionStorage for user data

--- dashboard_vendedora.js
+++ dashboard_vendedora.js
@@ -1,23 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const usuario = JSON.parse(localStorage.getItem("usuario"));
+    // OBTENER USUARIO
+    const usuario = JSON.parse(
+        sessionStorage.getItem("usuario")
+    );
 
+    // VALIDAR SESIÓN
     if (!usuario) {
-        window.location.href = "login.html";
+
+        window.location.href = "index.html";
+
         return;
+
     }
 
-    if (usuario.rol !== "vendedora") {
-        window.location.href = "login.html";
+    // VALIDAR ROL
+    if (
+        usuario.rol.toLowerCase()
+        !== "vendedora"
+    ) {
+
+        window.location.href = "index.html";
+
     }
 
 });
 
+
+// CERRAR SESIÓN
 function cerrarSesion(){
-    localStorage.removeItem("usuario");
-    window.location.href = "login.html";
+
+    sessionStorage.clear();
+
+    window.location.href = "index.html";
+
 }
 
+
+// VOLVER
 function volver(){
+
     window.history.back();
+
 }
