95ad029fd74c20e8669c34c04eadec2c58ff105f
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-05-18 14:07:36 -0500

    Update API endpoints and user session validation

--- ventas.js
+++ ventas.js
@@ -1,11 +1,11 @@
 const API_USUARIOS =
-"https://api.sheety.co/439db015c12617013288a2fb34648f06/bdFinal/usuarios";
+"https://api.sheety.co/301327363ae1c8d017800bb4566af87c/bdFinal/usuarios";
 
 const API_PRODUCTOS =
-"https://api.sheety.co/439db015c12617013288a2fb34648f06/bdFinal/productos";
+"https://api.sheety.co/301327363ae1c8d017800bb4566af87c/bdFinal/productos";
 
 const API_VENTAS =
-"https://api.sheety.co/439db015c12617013288a2fb34648f06/bdFinal/ventas";
+"https://api.sheety.co/301327363ae1c8d017800bb4566af87c/bdFinal/ventas";
 
 
 // USUARIO ACTIVO
