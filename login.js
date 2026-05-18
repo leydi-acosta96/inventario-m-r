39e3ecdb205920174335b218c6bb142d52c5a085
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-05-18 14:18:12 -0500

    Update API URL in login.js

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const URL = "https://api.sheety.co/439db015c12617013288a2fb34648f06/bdFinal/usuarios";
+const URL = "https://api.sheety.co/301327363ae1c8d017800bb4566af87c/bdFinal/usuarios";
 
 document.getElementById("loginForm").addEventListener("submit", login);
 
