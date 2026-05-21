c02ebe46e3ff0275f1019587a89c0e5badd4984d
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-05-21 10:22:35 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,25 +1,74 @@
 function obtenerUsuario(){
-  return JSON.parse(sessionStorage.getItem("usuario"));
+
+  return JSON.parse(
+    sessionStorage.getItem("usuario")
+  );
+
 }
 
+
 function validarSesion(){
+
   const usuario = obtenerUsuario();
 
   if(!usuario){
-    window.location.href = "../index.html";
+
+    window.location.href = "./index.html";
+
   }
+
 }
+
 
 function cerrarSesion(){
+
   sessionStorage.removeItem("usuario");
-  window.location.href = "../index.html";
+
+  window.location.href = "./index.html";
+
 }
 
-function validarRol(rolPermitido){
+
+// PERMITE UNO O VARIOS ROLES
+function validarRol(rolesPermitidos){
+
   const usuario = obtenerUsuario();
 
-  if(usuario.rol !== rolPermitido){
-    alert("No tienes acceso a esta sección");
-    window.location.href = "../index.html";
+  // VALIDAR SESIÓN
+  if(!usuario){
+
+    window.location.href = "./index.html";
+
+    return;
+
   }
+
+  // SI ENVÍAN SOLO UN ROL
+  if(typeof rolesPermitidos === "string"){
+
+    rolesPermitidos =
+    [rolesPermitidos];
+
+  }
+
+  // VALIDAR ACCESO
+  if(
+
+    !rolesPermitidos.includes(
+
+      usuario.rol.toLowerCase()
+
+    )
+
+  ){
+
+    alert(
+      "No tienes acceso a esta sección"
+    );
+
+    window.location.href =
+    "./index.html";
+
+  }
+
 }
